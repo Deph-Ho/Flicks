941a97d48866b9e7a095bb6a41a9a5ffee15a5e1
Dephanie Ho <dephho88@gmail.com>
2017-01-31 21:41:57 -0800

    Finished basic flicks app

--- Flicks/Flicks/MoviesViewController.swift
+++ Flicks/Flicks/MoviesViewController.swift
@@ -22,15 +22,21 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.delegate = self
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = NSURLRequest(URL: url, cachePolicy: .ReloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        //let session = NSURLSession(configuration: NSURLSessionConfiguration.default, delegate: nil, delegateQueue: NSOperationQueue.main)
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
+        
+        
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request) { (data, response, error) in
             if let data = data {
-                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                if let dataDictionary = try! NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary {
                     print(dataDictionary)
                     
-                    self.movies = dataDictionary["results"] as! [NSDictionary]
+                    
+                    self.movies = dataDictionary["results"] as? [NSDictionary]
                     self.tableView.reloadData()
                 }
             }
@@ -52,7 +58,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         } else {
            return 0
         }
-        return movies.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
@@ -63,15 +68,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let overview = movie["overview"] as! String
         let posterPath = movie["poster_path"] as! String
         
-        let baseUrl = "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
-        let imageUrl = NSURL(string: baseUrl + posterPath)
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        if let imageUrl = NSURL(string: baseUrl + posterPath){
+           cell.posterImageView.setImageWithURL(imageUrl)
+        }
         
-        cell.posterView = setImage
+     
         
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        cell.posterView.setImageWithURL(imageURL!)
+//        cell.posterView.setImageWithURL(imageURL!)
         
         
         
