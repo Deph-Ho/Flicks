dd49e8dcd285501b9a8693789d7e880799ff3cc7
Dephanie Ho <dephho88@gmail.com>
2017-02-05 12:39:03 -0800

    Finished basics

--- Flicks/Flicks/MoviesViewController.swift
+++ Flicks/Flicks/MoviesViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AFNetworking
+import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -21,26 +22,39 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         
+        //refresh control
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_refreshControl:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
-        let request = NSURLRequest(URL: url, cachePolicy: .ReloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         
         //let session = NSURLSession(configuration: NSURLSessionConfiguration.default, delegate: nil, delegateQueue: NSOperationQueue.main)
-        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
         
+        //Display HUD right before the request is made 
+        MBProgressHUD.showAdded(to: self.view, animated: true)
         
-        
-        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request) { (data, response, error) in
+        let task: URLSessionDataTask = session.dataTask(with: request, completionHandler: { (data, response, error) in
             if let data = data {
-                if let dataDictionary = try! NSJSONSerialization.JSONObjectWithData(data, options: []) as? NSDictionary {
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     print(dataDictionary)
                     
+                    //Hide HUD once the network request comes back
+                    MBProgressHUD.hide(for: self.view, animated: true)
                     
                     self.movies = dataDictionary["results"] as? [NSDictionary]
+                    
+                    //Reload the tableView for new data
                     self.tableView.reloadData()
+                    
+                    //refreshControl.endRefreshing()
+                    
                 }
             }
-        }
+        })
         task.resume()
         
 
@@ -52,7 +66,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         if let movies = movies{
            return movies.count
         } else {
@@ -60,8 +74,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         }
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
@@ -69,24 +83,46 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let posterPath = movie["poster_path"] as! String
         
         let baseUrl = "https://image.tmdb.org/t/p/w500"
-        if let imageUrl = NSURL(string: baseUrl + posterPath){
-           cell.posterImageView.setImageWithURL(imageUrl)
+        if let imageUrl = URL(string: baseUrl + posterPath){
+           cell.posterImageView.setImageWith(imageUrl)
         }
-        
-     
-        
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-//        cell.posterView.setImageWithURL(imageURL!)
-        
-        
-        
         
         print("row\(indexPath.row)")
         return cell
     }
+  
+    func refreshControlAction(_refreshControl: UIRefreshControl)
+    {
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+         
+        //let session = NSURLSession(configuration: NSURLSessionConfiguration.default, delegate: nil, delegateQueue: NSOperationQueue.main)
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
+         
+        let task: URLSessionDataTask = session.dataTask(with: request, completionHandler: { (data, response, error) in
+        if let data = data {
+        if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+        print(dataDictionary)
+         
+        self.movies = dataDictionary["results"] as? [NSDictionary]
+         
+        //Reload the tableView for new data
+        self.tableView.reloadData()
+         
+        _refreshControl.endRefreshing()
+         
+                }
+            }
+        })
+        task.resume()
 
+    
+    }
     /*
     // MARK: - Navigation
 
